f4569245ad7e6496ffafb03be0798bb2d23dbe36
ahmed14-cell <ahmedito14@gmail.com>
2020-11-10 00:31:14 +0300

    Completed Elasticnet Experiment

--- produce/train_model.py
+++ produce/train_model.py
@@ -2,7 +2,7 @@ import pickle
 
 import pandas as pd
 from config import Config
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import ElasticNet
 
 # Creating a path to save the models
 Config.models_path.mkdir(parents=True,exist_ok=True)
@@ -12,9 +12,7 @@ x_train = pd.read_csv(str(Config.features_path / 'train_features.csv'))
 y_train = pd.read_csv(str(Config.features_path / 'train_target.csv'))
 
 # Instantiating and fitting the data with the algorithm
-model = RandomForestRegressor(
-    n_estimators=150, max_depth=6, random_state=Config.random_seed
-)
+model = ElasticNet()
 model = model.fit(x_train, y_train.to_numpy().ravel())
 
 # Saving the model in a pickle file
