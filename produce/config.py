a6d6f32cceb35c45efef6ae4ae406c4fae326d2f
ahmed14-cell <ahmedito14@gmail.com>
2020-11-09 23:23:48 +0300

    Complete LR experiment

--- produce/config.py
+++ produce/config.py
@@ -3,8 +3,8 @@ from pathlib import Path
 class Config:
     random_seed = 42
     assets_path = Path('./assets')
-    original_dataset_path = assets_path / 'datasets' / 'dataset1.csv'
-    dataset_path = assets_path / 'data'
-    features_path = assets_path / 'features'
-    models_path = assets_path / 'models'
-    metrics_file_path = assets_path / 'metrics.json'
+    original_dataset_path = assets_path / 'original_dataset' / 'dataset1.csv'
+    dataset_path = assets_path / 'data_set'
+    features_path = assets_path / 'feature'
+    models_path = assets_path / 'model'
+    metrics_file_path = assets_path / 'metric.json'
